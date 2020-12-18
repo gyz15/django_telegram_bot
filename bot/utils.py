f0261a9b9fca17102d70e46d1f3a5f59f5219137
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2020-12-18 22:55:22 +0800

    Shout back user

---
+++ bot/utils.py
@@ -0,0 +1,19 @@
+import urllib
+from os import environ
+import requests
+from decouple import config
+
+TOKEN = config('TOKEN')
+URL = f"https://api.telegram.org/bot{TOKEN}/"
+
+
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + f"sendMessage?text={text}&chat_id={chat_id}"
+    get_url(url)
+
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
--- bot/views.py
+++ bot/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-
+from .utils import send_message
 
 # Create your views here.
+
+
 def main(request):
     data = json.loads(request.body)
-    # todo utils send the shit back
+    print(data)
+    try:
+        text = data["message"]["text"]
+        chat_id = data["message"]["chat"]["id"]
+        send_message(text, chat_id)
+    except KeyError:
+        print("Something I don't know")
     return JsonResponse({"ok": "POST request processed"})
