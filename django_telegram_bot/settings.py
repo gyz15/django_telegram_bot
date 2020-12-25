6653ad2577da4e0cc91c047f5f4432beb37d1a59
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2020-12-25 11:51:40 +0800

    staticfiles for heroku?

--- django_telegram_bot/settings.py
+++ django_telegram_bot/settings.py
@@ -15,6 +15,9 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,5 +127,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
