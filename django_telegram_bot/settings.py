a89c319462212e6a3bda9eb90b353a73c573cda1
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2020-12-25 11:39:49 +0800

    Database using dj_database_url

--- django_telegram_bot/settings.py
+++ django_telegram_bot/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -123,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
