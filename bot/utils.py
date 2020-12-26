f0145d878e82caedd7fde627e3e7faf5404262d5
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2020-12-26 19:33:54 +0800

    Different bot and debug = True while not on heroku

--- bot/utils.py
+++ bot/utils.py
@@ -7,8 +7,12 @@ from .models import TGUser, Action
 # from time import sleep
 from math import floor, log10
 
-DEPLOY_BOT_TOKEN = config('DEPLOY_BOT_TOKEN')
-URL = f"https://api.telegram.org/bot{DEPLOY_BOT_TOKEN}/"
+if config('ON_HEROKU',cast=int):
+    BOT_TOKEN = config('DEPLOY_BOT_TOKEN')
+else:
+    BOT_TOKEN = config('TEST_BOT_TOKEN')
+
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/"
 
 
 def send_message(text, chat_id):
