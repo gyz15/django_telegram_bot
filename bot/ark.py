5c6f6d4a4d9ddb1df71288dcca70cd28222c5745
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2021-01-01 16:19:57 +0800

    Handling data sending to user and also changing database

--- bot/ark.py
+++ bot/ark.py
@@ -3,12 +3,11 @@ from .utils import send_message
 import pandas as pd
 import requests
 import os
+import math
 
 
 def main():
     for etf in ArkFund.objects.all():
-        sending_data = {"added": [], "removed": [],
-                        "buying": [], "selling": []}
         with requests.get(etf.file_url, stream=True) as r:
             with open(f".\{etf.ticker}.csv", "wb") as f:
                 for chunk in r.iter_content(chunk_size=8192):
@@ -16,30 +15,73 @@ def main():
                         f.write(chunk)
         new_data = pd.read_csv(
             f".\{etf.ticker}.csv", parse_dates=[0], dayfirst=True)
-        # comparing
+        sending_data = {"added": [], "removed": [],
+                        "buying": [], "selling": []}
+        # Comparing latest data with the data in the database
         for company_name in new_data.company:
+            new_company = new_data.loc[new_data.company == company_name]
             try:
                 stock = etf.stocks.get(company=company_name)
-                if new_data.loc[new_data.company == company_name]['shares'] != stock.shares:
-                    # owh oh, fund manager added or decrease the share amount that they hold
-                    # hanlde increase/decrease of shares
-                    # add to the message to sent to user
-                    pass
-                else:
-                    # no changes on stock, passs
-                    pass
+                sending_data, stock = handle_stock_add_minus(
+                    sending_data, new_company, stock)
                 stock.had_changes = True
                 stock.save()
             except ArkStock.DoesNotExist:
-                # New stock added, handle it(database)
-                # And also message
-                pass
+                # todo handle message here
+                fund = new_company['fund']
+                company = new_company['company']
+                ticker = new_company['ticker']
+                shares = new_company['shares']
+                weight = new_company['weight(%)']
+                stock = ArkStock.objects.create(
+                    company=company, ticker=ticker, shares=shares, weight=weight)
+                stock.save()
+                fund = ArkFund.objects.get(ticker=fund)
+                fund.stocks.add(stock)
+                fund.save()
+                # data = []
+                # append
+                # sending_data['added'] =
         removed_stocks = etf.stocks.filter(had_changes=False)
         for stock in removed_stocks:
-            # do something for removed stocks
+            sending
+            stock.delete()
             pass
         if os.path.exists(f".\{etf.ticker}.csv"):
             os.remove(f".\{etf.ticker}.csv")
         else:
             pass
-            # raise an error or send a message to admin
+        # todo send message for all user that subscribed to this fund
+
+
+def handle_stock_add_minus(sending_data, new_company, stock):
+    add = True
+    if new_company['shares'] > stock.shares:
+        add = True
+    elif new_company['shares'] < stock.shares:
+        add = False
+    else:
+        add = None
+    if add is not None:
+        stock.shares_delta = (
+            (new_company['shares']) - (stock.shares))
+        stock.shares_delta_percent = (
+            math.round(stock.shares_delta/stock.shares), 2)
+        stock.shares = new_company['shares']
+        stock.weight_delta = (
+            (new_company['weight(%)']) - (stock.weight))
+        stock.weight = new_company['weight(%)']
+        data = []
+        data.append(stock.company)
+        data.append(stock.ticker)
+        data.append(stock.shares)
+        data.append(stock.shares_delta_percent)
+        data.append(stock.weight)
+        data.append(stock.weight_delta)
+        if add:
+            sending_data['buying'].append(data)
+        else:
+            sending_data['selling'].append(data)
+    else:
+        pass
+    return sending_data, stock
