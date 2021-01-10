36a5a3b6ff936a1e39077a16d6dc6658073496e4
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2021-01-10 22:17:04 +0800

    Full headers

--- bot/utils.py
+++ bot/utils.py
@@ -184,13 +184,24 @@ def get_stock(symbol):
     ua = UserAgent()
     if config('ON_HEROKU', cast=int):
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'no-cache',
+            'cookie': 'machine_cookie=3977770892613',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1'
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+        }'
         data = requests.get(
             f"https://seekingalpha.com/api/v3/symbols/{symbol}/data", headers=headers)
         print(data.text)
     else:
         headers = {
-            'User-Agent': f'{str(ua.Chrome)}'}
+            'User-Agent': f'{str(ua.random)}'}
         data = requests.get(
             f"https://seekingalpha.com/api/v3/symbols/{symbol}/data", headers=headers)
     return data.json()
