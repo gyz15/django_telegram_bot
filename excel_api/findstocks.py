6d91eef38f386807a6c4babfdfa01b30a83ab886
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2021-02-03 17:18:19 +0800

    Setup flow, doing frontend work on excel

--- excel_api/findstocks.py
+++ excel_api/findstocks.py
@@ -1,1 +1,51 @@
 # TODO a function that can fetch data and process it into json object
+import requests
+
+
+def is_valid_stock(stock):
+    '''Check user submission validity'''
+    if len(stock) <= 5 and stock.isalpha() adn len(stock) > 0:
+        return True
+    return False
+
+
+def find_stock(stock):
+    '''Main function '''
+    return_res = {}
+    if not is_valid_stock(stock):
+        return {"Error": "Object is not a valid stock symbol"}
+    fetch_from_sa()
+    # fetch from sa
+    # if sa data no prob
+    # try clean
+    # real time price
+    # no prob clean
+    # combine data
+    # calc call_left
+    # return response
+    user_obj.call_used_today += 1
+    user_obj.save(update_fields=['call_used_today'])
+    call_left = user_obj.plan.api_per_day - user_obj.call_used_today
+
+
+def fetch_from_sa(url):
+    '''Fetch data from SA'''
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+    }
+    data = requests.get(url, headers=headers).json()
+    if "errors" in data:
+        return {"Errors": data['errors'][0]['detail']}
+    return data
+
+
+def clean_data(data):
--- excel_api/views.py
+++ excel_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import ApiUser, Plan
 from .utils import is_valid_key
+from .findstocks import find_stocks
 
 # Create your views here.
 
@@ -11,17 +12,18 @@ from .utils import is_valid_key
 class FindStocks(APIView):
     def get(self, request, format=None):
         print(request.headers)
-        if 'API-Key' in request.headers:
+        if 'API-Key' in request.headers and 'Stock' in request.headers:
             valid, user_obj = is_valid_key(request.headers['API-Key'])
             if valid:
                 if user_obj.has_call:
-                    user_obj.call_used_today += 1
-                    user_obj.save(update_fields=['call_used_today'])
-                    call_left = user_obj.plan.api_per_day - user_obj.call_used_today
-                    return Response({"user_data": {'Call left': call_left}, "stock_data": {"pe": 123, "marketCap": 456}}, status=status.HTTP_200_OK)
+                    stock_response = find_stocks(
+                        request.headers['Stocks'], user_obj)
+                    if "Error" in stock_response.keys():
+                        return Response(stock_response, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(stock_response, status=status.HTTP_200_OK)
                 else:
                     return Response({'Error': 'Call exceeded'}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 return Response({'Error': 'Key invalid'}, status=status.HTTP_404_NOT_FOUND)
         else:
-            return Response({'Error': 'Key Not Included'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'Error': 'Key or Stock Symbol not included'}, status=status.HTTP_400_BAD_REQUEST)
